eea0ec5b9063088fd5727a7e35d4e89e9620e008
GibonKonrad <konrad201020@outlook.com>
2024-08-09 00:48:22 +0200

    wskaznik this

--- lesson1/stock00.cpp
+++ lesson1/stock00.cpp
@@ -2,7 +2,6 @@
 #include"stocks00.h"
 Stock::Stock()
 {
-    std::cout<<"Wywolano konsturkotor domyslny\n";
     company="bez nazwy";
     shares=0;
     share_val=0.0;
@@ -10,7 +9,6 @@ Stock::Stock()
 }
 Stock::Stock(const std::string & co, long n, double pr)
 {
-    std::cout<<"Wywolano konstuktor z argumentem "<<co<<'\n';
     company=co;
     if(n<0)
     {
@@ -22,6 +20,9 @@ Stock::Stock(const std::string & co, long n, double pr)
         shares=n;
     share_val=pr;
     set_tot();
+}
+Stock::~Stock(){
+
 }
 void Stock::buy(long num, double price)
 {
@@ -76,4 +77,11 @@ void Stock::show()
     cout<<" Laczna wartosc udzialow: "<<total_val<<" zl"<<'\n';
     cout.setf(orig, ios_base::floatfield);
     cout.precision(prec);
+}
+const Stock & Stock::topval(const Stock & s) const
+{
+    if(s.total_val>total_val)
+        return s;
+    else 
+        return *this;
 }
--- lesson1/stocks00.h
+++ lesson1/stocks00.h
@@ -17,6 +17,7 @@ class Stock
         void buy(long num, double price);
         void sell(long num, double price);
         void update(double price);
-        void show();      
+        void show();  
+        const Stock & topval(const Stock & s) const;    
 };
 #endif
